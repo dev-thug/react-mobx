f216f6cba03b736ac1e41a34e89f8a529912d40b
Hyunjoong Kim <de0978@gmail.com>
2022-02-12 23:14:23 +0900

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,5 @@
-import { observer } from "mobx-react";
-import countStore from "./store/count";
-
-const App: React.FC = () => {
-  const count = countStore;
-
-  return (
-    <div style={{ padding: "50px" }}>
-      <div style={{ marginBottom: "50px" }}>
-        <h1>Count (Class)</h1>
-        <div>number: {count.number}</div>
-        <button onClick={() => count.increase()}>plus</button>
-        <button onClick={() => count.decrease()}>minus</button>
-      </div>
-
-      {/* <div style={{ marginBottom: "50px" }}>
-        <h1>Count (Object)</h1>
-        <div>num: {count.num}</div>
-        <button onClick={() => count.increase()}>increment</button>
-      </div> */}
-    </div>
-  );
+const App = () => {
+  return <div></div>;
 };
 
-export default observer(App);
+export default App;
