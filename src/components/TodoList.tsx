c4f166e2cd62d80ee56a002ca8c7243bcd8d58cf
Hyunjoong Kim <de0978@gmail.com>
2022-02-13 22:25:24 +0900

    update design

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,48 @@
-import { observer } from "mobx-react"
-import { useContext } from "react"
-import TodoStore from "../stores/TodoStore"
+import { observer } from "mobx-react";
+import { useContext, useState } from "react";
+import TodoStore from "../stores/TodoStore";
 
-const TodoList = () =>{
-    const todoStore = useContext(TodoStore)
-    const {todos,toggleTodo, removeTodo} = todoStore
+const TodoList = () => {
+  const todoStore = useContext(TodoStore);
+  const { todos, toggleTodo, removeTodo } = todoStore;
+  const [title, setTitle] = useState("");
 
-    return <>
-    {todos.map(todo => (
+  const { addTodo, info } = todoStore;
+  return (
+    <>
+      {todos.map((todo) => (
         <>
-        <p>{todo.title}</p>
-        <p>{todo.completed}</p>
-        <button onClick={_=> toggleTodo(todo.id!)}>Toggle</button>
-        <button onClick={_=> removeTodo(todo.id!)}>Remove</button>
+          <p>{todo.title}</p>
+          <p>{todo.completed}</p>
+          <button onClick={(_) => toggleTodo(todo.id!)}>Toggle</button>
+          <button onClick={(_) => removeTodo(todo.id!)}>Remove</button>
+         
         </>
-    ))}
+      ))}
+       <>
+            <p>Total items: {info.total}</p>
+            <p>Finished items: {info.completed}</p>
+            <p>Unfinished items: {info.notCompleted}</p>
+            <br />
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <button
+              onClick={(_) => {
+                addTodo({
+                  title: title,
+                  completed: false,
+                });
+                setTitle("");
+              }}
+            >
+              ADD
+            </button>
+          </>
     </>
-}
+  );
+};
 
 export default observer(TodoList);
