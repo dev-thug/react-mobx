720bafb4868f7086f50252ddec7e8844ca7e01a9
Hyunjoong Kim <de0978@gmail.com>
2022-04-03 21:19:17 +0900

    add https://ko.mobx.js.org/README.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import AddTodo from "./components/AddTodo";
-import TodoList from "./components/TodoList";
+import { makeAutoObservable } from "mobx";
+import { observer } from "mobx-react";
+
+class Timer {
+  secondsPassed = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increase() {
+    this.secondsPassed += 1;
+  }
+
+  reset() {
+    this.secondsPassed = 0;
+  }
+}
+
+const myTimer = new Timer()
+
+const TimerView:React.FC<{timer:Timer}> = observer((props) => (
+  <button onClick={() => props.timer.reset()}>Seconds Pass : {props.timer.secondsPassed}</button>
+))
 
 const App = () => {
-  return (
-    <div>
-      <TodoList/>
-      <AddTodo />
-    </div>
-  );
+
+  setInterval(()=>{
+    myTimer.increase()
+  }, 1000)
+
+  return <TimerView timer={myTimer}></TimerView>;
 };
 
 export default App;
