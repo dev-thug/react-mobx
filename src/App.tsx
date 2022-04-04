b3c09140ed2992d7530236a38a6c7ee3c9961773
Hyunjoong Kim <de0978@gmail.com>
2022-04-04 09:52:31 +0900

    add https://ko.mobx.js.org/the-gist-of-mobx.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { makeAutoObservable } from "mobx";
+import { action, computed, makeObservable, observable } from "mobx";
 import { observer } from "mobx-react";
 
-class Timer {
-  secondsPassed = 0;
+class Todo {
+  id = Math.random();
+  title = "";
+  finished = false;
 
-  constructor() {
-    makeAutoObservable(this);
+  constructor(title: string) {
+    makeObservable(this, {
+      title: observable,
+      finished: observable,
+      toggle: action,
+    });
+    this.title = title;
   }
 
-  increase() {
-    this.secondsPassed += 1;
-  }
-
-  reset() {
-    this.secondsPassed = 0;
+  //   state를 변경하는 메서드는 MobX 용어로 action
+  toggle() {   
+    this.finished = !this.finished;
   }
 }
 
-const myTimer = new Timer()
+class TodoList {
+  todos: Todo[] = [];
 
-const TimerView:React.FC<{timer:Timer}> = observer((props) => (
-  <button onClick={() => props.timer.reset()}>Seconds Pass : {props.timer.secondsPassed}</button>
-))
+  get unfinishedTodoCount() {
+    return this.todos.filter((todo) => !todo.finished).length;
+  }
+
+  constructor(todos: Todo[]) {
+    makeObservable(this, {
+      todos: observable,
+      unfinishedTodoCount: computed,
+    });
+    this.todos = todos;
+  }
+
+  
+}
+
+const TodoView: React.FC<{ todo: Todo }> = observer((props) => {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        defaultChecked={props.todo.finished}
+        
+        onClick={() => props.todo.toggle()}
+      />
+      {props.todo.title}
+    </li>
+  );
+});
+
+const TodoListView: React.FC<{ todoList: TodoList }> = observer((props) => {
+  return (
+    <div>
+      <ul>
+        {props.todoList.todos.map((todo) => (
+          <TodoView key={todo.id} todo={todo} />
+        ))}
+      </ul>
+      Tasks left : {props.todoList.unfinishedTodoCount}
+    </div>
+  );
+});
+
+const store = new TodoList([
+  new Todo("Get Coffee"),
+  new Todo("Write simpler code"),
+]);
 
 const App = () => {
-
-  setInterval(()=>{
-    myTimer.increase()
-  }, 1000)
-
-  return <TimerView timer={myTimer}></TimerView>;
+  return <TodoListView todoList={store} />;
 };
 
 export default App;
